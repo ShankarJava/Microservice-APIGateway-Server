22561ad5e373e8034adab36907ccf57d4052632d
ShankarTogari <ShankarThogarii@gmail.com>
2023-01-25 19:39:55 +0530

    MSAPI-Server

---
+++ src/main/java/org/shan/sfw/boot/micro/service/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package org.shan.sfw.boot.micro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Eureka Server");
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
